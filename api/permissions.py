1089696ef24bf3ce6873f621f9746f11ffad2ae4
Vlad M <vladpochta5@gmail.com>
2017-09-10 13:49:49 +0300

    Remove permissions.py

--- api/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-
-class ReadUpdateDeletePerm(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.method == 'POST'
